65163118c224e1f9598836326deec28c7f431b10
hanzala-sohrab <sorb.hanzala@gmail.com>
2020-12-01 22:59:48 +0530

    Add declare.sh

--- Variables, Control Structures, and Arithmetic/declare.sh
+++ Variables, Control Structures, and Arithmetic/declare.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 declare -l lstring="ABCdef"
 declare -u ustring="ABCdef"
+declare -r readonly="A Value"
+declare -a Myarray
+declare -A Myarray1
+
+echo lstring = $lstring
+echo ustring = $ustring
+echo readonly = $readonly
+readonly="new val"
+Myarray[2]="2nd val"
+echo 'Myarray[2]= ' ${Myarray[2]}
+Myarray1["hotdog"]="baseball"
+echo 'Myarray1[hotdog]= ' ${Myarray1["hotdog"]}
